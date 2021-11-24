2b60e0b9ce7e852204eadd1ee14e83f30982ddea
lcs-ashin <ashin@lcs.on.ca>
2021-11-24 11:31:47 -0500

    Initial Commit

---
+++ PlaylistGenerator/PlaylistGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistGeneratorApp.swift
+//  PlaylistGenerator
+//
+//  Created by Eunbi Shin on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
