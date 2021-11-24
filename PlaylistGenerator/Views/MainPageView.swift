312e0dd7a2f4b0252a12821f01d854ecd6cbfc0f
lcs-ashin <ashin@lcs.on.ca>
2021-11-24 12:20:53 -0500

    Changed the name of the main page to "MainPageView"

--- PlaylistGenerator/Views/MainPageView.swift
+++ PlaylistGenerator/Views/MainPageView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainPageView.swift
 //  PlaylistGenerator
 //
 //  Created by Eunbi Shin on 2021-11-24.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainPageView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainPageView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainPageView()
     }
 }
