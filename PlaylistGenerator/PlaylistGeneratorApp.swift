58961f76f363bab2f66df8974a9d1802478710d9
lcs-ashin <ashin@lcs.on.ca>
2021-11-28 15:21:26 -0500

    Made the front page of the app. (Label & Buttons)

--- PlaylistGenerator/PlaylistGeneratorApp.swift
+++ PlaylistGenerator/PlaylistGeneratorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PlaylistGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainPageView()
         }
     }
 }
--- PlaylistGenerator/Views/MainPageView.swift
+++ PlaylistGenerator/Views/MainPageView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct MainPageView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            // Title
+            Label(title: {
+                Text("Playlist Generator")
+            }, icon: {
+                Image(systemName: "music.note.list")
+            })
+                .font(.title.bold())
+            
+            // Insert button
+            Group {
+                Button(action: {
+                    // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                    print("Button was pressed")
+                }, label: {
+                    Text("Create a new playlist")
+                })
+                
+                Button(action: {
+                    print("Button was pressed")
+                }, label: {
+                    Text("View my playlists")
+                })
+            }
+            .buttonStyle(.bordered)
+            .font(.title3.bold())
+            .foregroundColor(.black)
+        }
     }
 }
 
