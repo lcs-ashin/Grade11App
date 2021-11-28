dca0ab60cfbddea487eeb28910af0ea24d5f40f0
lcs-ashin <ashin@lcs.on.ca>
2021-11-28 17:50:32 -0500

    Added a share button to "MyPlaylistDetailView" and changed the stack of texts to the list

--- PlaylistGenerator/Views/MyPlaylistDetailView.swift
+++ PlaylistGenerator/Views/MyPlaylistDetailView.swift
@@ -11,17 +11,24 @@ struct MyPlaylistDetailView: View {
     
     let thisPlaylist: Playlist
     var body: some View {
-        VStack(alignment: .leading) {
-            Group {
-                HStack {
-                    Text("Song Title 1")
-                    Spacer()
-                }
+        VStack(alignment: .center) {
+            List {
+                Text("Song Title 1")
                 Text("Song Title 2")
                 Text("Song Title 3")
             }
-            .font(Font.custom("Helvetica", size: 30))
+            .font(Font.custom("Helvetica", size: 25))
+            .listStyle(.plain)
             Spacer()
+            
+            // Share Button
+            Button(action: {
+                print("Share")
+            }, label: {
+                Image(systemName: "square.and.arrow.up.circle")
+            })
+                .foregroundColor(.black)
+                .font(.system(size: 40))
         }
         .padding()
         .navigationTitle(thisPlaylist.playlistTitle)
